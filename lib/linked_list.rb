c8ae68a1333bb29de8acb45f26a0fcbce0f4ed14
katyjane8 <katy.jane8@gmail.com>
2017-10-08 13:26:00 -0600

    iteration 1 complete

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,12 +1,35 @@
+require_relative 'node'
+require 'pry'
+
 class LinkedList
-  attr_accessor :head
+  attr_accessor :head, :node
   def initialize(head = nil)
     @head = head
-    @surname = []
-    @next_node = nil
+    @count = 0
   end
 
   def append(surname)
+    if @head == nil
+      @head = Node.new(surname)
+    elsif node.next_node.nil?
+      node.next_node = Node.new(surname)
+    else
+      append(surname)
+    end
+  end
+
+  def count
+  current_node = @head
+    until current_node.nil?
+      @count += 1
+      current_node = current_node.next_node
+    end
+    @count
+  end
+
+  def to_string
+    current_node = @head
+    family_list = "The #{head.surname} family"
   end
 
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -7,31 +7,46 @@ require './lib/linked_list'
 class LinkedListTest < Minitest::Test
   def test_linked_list_exists
     list = LinkedList.new
+
     assert_instance_of LinkedList, list
   end
 
   def test_linked_list_head_is_nil
     list = LinkedList.new
+
     assert_nil list.head
   end
 
   def test_first_node_can_change
-    list = LinkedList.new("West")
-    assert_equal , list.append("West")
+    list = LinkedList.new
+
+    assert_instance_of Node, list.append("West")
   end
 
   def test_linked_list_holds_instance_of_node
     list = LinkedList.new("West")
+
     assert_instance_of LinkedList, list
   end
 
   def test_node_after_head_is_nil
-    list = LinkedList.new("West")
+    list = LinkedList.new
+    list.append("West")
+
     assert_nil list.head.next_node
   end
 
-# > list.count
-# => 1
-# > list.to_string
-# => "The West family"
+  def test_list_has_a_count
+    list = LinkedList.new
+    list.append("West")
+
+    assert_equal 1, list.count
+  end
+
+  def test_list_can_be_string
+    list = LinkedList.new
+    list.append("West")
+
+    assert_equal "The West family", list.to_string
+  end
 end
